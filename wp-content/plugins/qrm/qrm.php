c2d00e005c3dfc2d473afaa7abfa7b70d6bad8da
daveontour <dave_on_tour@yahoo.com>
2016-09-27 12:35:33 +0400

    Table Creation
    Creation of custome tables in activation

--- wp-content/plugins/qrm/qrm.php
+++ wp-content/plugins/qrm/qrm.php
@@ -18,6 +18,53 @@ if (! defined ( 'WPINC' )) {
 define('QRM_VERSION', '1.3.3');
 defined ( 'ABSPATH' ) or die ();
 
+abstract class WPQRM_Model {
+    static $primary_key = 'id';
+    private static function _table() {
+        global $wpdb;
+        $tablename = strtolower( get_called_class() );
+        $tablename = str_replace( 'wpqrm_model_', 'wpqrm_', $tablename );
+        return $wpdb->prefix . $tablename;
+    }
+    private static function _fetch_sql( $value ) {
+        global $wpdb;
+        $sql = sprintf( 'SELECT * FROM %s WHERE %s = %%s', self::_table(), static::$primary_key );
+        return $wpdb->prepare( $sql, $value );
+    }
+    static function get( $value ) {
+        global $wpdb;
+        return $wpdb->get_row( self::_fetch_sql( $value ) );
+    }
+    static function insert( $data ) {
+        global $wpdb;
+        $wpdb->insert( self::_table(), $data );
+    }
+    static function update( $data, $where ) {
+        global $wpdb;
+        $wpdb->update( self::_table(), $data, $where );
+    }
+    static function delete( $value ) {
+        global $wpdb;
+        $sql = sprintf( 'DELETE FROM %s WHERE %s = %%s', self::_table(), static::$primary_key );
+        return $wpdb->query( $wpdb->prepare( $sql, $value ) );
+    }
+    static function insert_id() {
+        global $wpdb;
+        return $wpdb->insert_id;
+    }
+    static function time_to_date( $time ) {
+        return gmdate( 'Y-m-d H:i:s', $time );
+    }
+    static function now() {
+        return self::time_to_date( time() );
+    }
+    static function date_to_time( $date ) {
+        return strtotime( $date . ' GMT' );
+    }
+}
+class WPQRM_Model_Risk extends WPQRM_Model {
+    static $primary_key = 'order_id';
+}
 final class Project{
 	public $id;
 	public $title;
@@ -1939,8 +1986,7 @@ final class QRMReportData{
 		}
 	}
 }
-final class QRM_AutoUpdate
-{
+final class QRM_AutoUpdate{
 	private $current_version;
 	private $update_path;
 	private $plugin_slug;
